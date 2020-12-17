681bea39ca29bb56dc53aa22c3fea54ee0cf0a2c
Denis Potapov <fantom.r45@mail.ru>
2020-12-17 13:03:10 +0500

    Disposables

--- app/src/main/java/com/example/rxjavaexample/MainActivity.kt
+++ app/src/main/java/com/example/rxjavaexample/MainActivity.kt
@@ -5,16 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.functions.Predicate
 import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    val disposables = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
 
         val taskObservable: Observable<Task> = Observable
@@ -36,6 +39,7 @@ class MainActivity : AppCompatActivity() {
         taskObservable.subscribe(object : Observer<Task> {
             override fun onSubscribe(d: Disposable) {
                 Timber.d("onSubscribe: called")
+                disposables.add(d)
             }
 
             override fun onNext(task: Task) {
@@ -51,5 +55,11 @@ class MainActivity : AppCompatActivity() {
                 Timber.d("onComplete: called")
             }
         })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposables.clear()
+        Timber.d("onDestroy: cleared")
     }
 }
