e16c23091042fa3addff1fcec51bf161c6b0637c
Denis Potapov <fantom.r45@mail.ru>
2020-12-22 11:49:59 +0500

    interval() - RxJava

--- app/src/main/java/com/example/rxjavaexample/MainActivity.kt
+++ app/src/main/java/com/example/rxjavaexample/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.example.rxjavaexample
 
 import android.os.Bundle
-import android.widget.SearchView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rxjavaexample.databinding.ActivityMainBinding
-import com.jakewharton.rxbinding2.view.RxView
-import io.reactivex.*
+import io.reactivex.Observable
+import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
-import io.reactivex.functions.Function
+import io.reactivex.functions.Predicate
 import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
@@ -19,7 +17,6 @@ class MainActivity : AppCompatActivity() {
 
     private val disposables = CompositeDisposable()
     private lateinit var binding: ActivityMainBinding
-    var timeSinceLastRequest = System.currentTimeMillis()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,34 +25,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
 
-        RxView.clicks(binding.button)
-            .throttleFirst(4000, TimeUnit.MILLISECONDS)
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : Observer<Any> {
-                override fun onSubscribe(d: Disposable) {
-                    disposables.add(d)
-                }
-
-                override fun onNext(t: Any) {
-                    Timber.d("onNext: time last since request ${System.currentTimeMillis() - timeSinceLastRequest}")
-                    someMethod()
-                }
-
-                override fun onError(e: Throwable) {
-                }
-
-                override fun onComplete() {
+        val intervalObservable = Observable
+            .interval(1, TimeUnit.SECONDS)
+            .subscribeOn(Schedulers.io())
+            .takeWhile(object : Predicate<Long> {
+                override fun test(t: Long): Boolean {
+                    Timber.d("test: $t thread: ${Thread.currentThread().name}")
+                    return t <= 5
                 }
             })
-    }
+            .observeOn(AndroidSchedulers.mainThread())
 
-    private fun someMethod() {
-        timeSinceLastRequest = System.currentTimeMillis()
-        Toast.makeText(this, "you clicked the button", Toast.LENGTH_SHORT).show()
-    }
+        intervalObservable.subscribe(object : Observer<Long> {
+            override fun onSubscribe(d: Disposable) {
+            }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        disposables.clear()
+            override fun onNext(t: Long) {
+                Timber.d("onNext: $t")
+            }
+
+            override fun onError(e: Throwable) {
+            }
+
+            override fun onComplete() {
+            }
+        })
     }
 }
